dcf5b10ba42cdcfea53ea0c6a57a5aa0757f442b
Lionel Hercot <lhercot@cisco.com>
2020-10-05 22:39:09 -0700

    Fix remote state syntax

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ data "terraform_remote_state" "network" {
   backend = "remote"
   config = {
     organization = var.org
-    workspaces {
+    workspaces = {
       name = var.network_workspace
     }
   }
