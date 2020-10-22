53d0618b9b27cdf6d2315682ea7e29c6253c4365
Lionel Hercot <lhercot@cisco.com>
2020-10-22 05:08:39 -0700

    On-Prem only

--- main.tf
+++ main.tf
@@ -10,38 +10,30 @@ data "terraform_remote_state" "network" {
 
 terraform {
   required_providers {
-    aws = {
-      source = "hashicorp/aws"
+    vsphere = {
+      source = "hashicorp/vsphere"
     }
   }
+}
+
+provider "vsphere" {
+  # Configuration options
+  // Requires ENV variable VSPHERE_USER 
+  // Requires ENV variable VSPHERE_PASSWORD
+  // Requires ENV variable VSPHERE_SERVER
+
+  # If you have a self-signed cert
+  allow_unverified_ssl = true
 }
 
 # Deploy VM to On-Premises site
 module "demo_vm" {
   source  = "app.terraform.io/cisco-dcn-ecosystem/demo_vm/vsphere"
-  version = "0.0.1"
+  version = "0.0.2"
 
   vmware_dvs = data.terraform_remote_state.network.outputs.vmware_vds
   vmware_portgroup = data.terraform_remote_state.network.outputs.vmware_portgroup
   vm_name = "TF-Wordpress-DB"
   db_ip_address = var.db_ip_address
   db_gateway = data.terraform_remote_state.network.outputs.db_gateway
-}
-
-# Deploy VM to Azure site
-module "demo-azure" {
-  source  = "app.terraform.io/cisco-dcn-ecosystem/demo-mso/azurerm"
-  version = "0.0.1"
-
-  vm_name = "TF-Wordpress-Web"
-  db_ip_address = var.db_ip_address
-  subnet_name = data.terraform_remote_state.network.outputs.azure_subnet_name
-  virtual_network_name = data.terraform_remote_state.network.outputs.virtual_network_name
-  vm_resource_group_name = "VMs_WoS_TF-Hybrid_Cloud_VRF_westus"
-  net_resource_group_name = data.terraform_remote_state.network.outputs.resource_group_name
-}
-
-provider "aws" {
-  # Configuration options
-  region = "us-west-1"
 }
--- outputs.tf
+++ outputs.tf
@@ -2,13 +2,3 @@ output "db_ip_address" {
   value = module.demo_vm.ip_address
   description = "The private IP address of the DB VM"
 }
-
-output "azure_vm_public_ip_address" {
-  value = module.demo-azure.public_ip_address
-  description = "The public IP address of the VM"
-}
-
-output "azure_vm_private_ip_address" {
-  value = module.demo-azure.ip_address
-  description = "The private IP address of the VM"
-}
