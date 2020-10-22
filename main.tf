c68c43fc5458a00abfc992527f946958eaf2f355
Lionel Hercot <lhercot@cisco.com>
2020-10-22 05:23:37 -0700

    Adding Azure VM

--- main.tf
+++ main.tf
@@ -13,6 +13,9 @@ terraform {
     vsphere = {
       source = "hashicorp/vsphere"
     }
+    azurerm = {
+      source = "hashicorp/azurerm"
+    }
   }
 }
 
@@ -36,4 +39,25 @@ module "demo_vm" {
   vm_name = "TF-Wordpress-DB"
   db_ip_address = var.db_ip_address
   db_gateway = data.terraform_remote_state.network.outputs.db_gateway
+}
+
+provider "azurerm" {
+  features {}
+  // Requires ENV variable ARM_SUBSCRIPTION_ID
+  // Requires ENV variable ARM_CLIENT_ID
+  // Requires ENV variable ARM_CLIENT_SECRET
+  // Requires ENV variable ARM_TENANT_ID
+}
+
+# Deploy VM to Azure site
+module "demo-azure" {
+  source  = "app.terraform.io/cisco-dcn-ecosystem/demo-mso/azurerm"
+  version = "0.0.2"
+
+  vm_name = "TF-Wordpress-Web"
+  db_ip_address = var.db_ip_address
+  subnet_name = data.terraform_remote_state.network.outputs.azure_subnet_name
+  virtual_network_name = data.terraform_remote_state.network.outputs.virtual_network_name
+  vm_resource_group_name = "VMs_WoS_TF-Hybrid_Cloud_VRF_westus"
+  net_resource_group_name = data.terraform_remote_state.network.outputs.resource_group_name
 }
